4b06a25427eceb4f3a1d51bf91ce2f4ca73a19ca
thejenkdog <thejenkdog@gmail.com>
2020-01-17 16:52:06 -0800

    Add config folder

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://fatty:cupcake@weighttracker-ld45t.mongodb.net/test?retryWrites=true&w=majority"
-  //Make sure this is your own unique string
+    
+    // String pulled from mongoDB, cluster view --> connect --> connect your application
 };
