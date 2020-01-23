cd96d6405d67d0e84284d533cb774f8269bc7998
thejenkdog <thejenkdog@gmail.com>
2020-01-22 20:15:00 -0800

    Add routes for registration and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,72 @@
 const express = require("express");
 const router = express.Router();
 
+// Import validations
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+// Test route
 router.get("/test", (req, res) => res.json({msg: "this is the users route"}))
+
+// Registration route
+router.post("/register", (req, res) => {
+  
+  // Deconstruct response coming back from validations
+  // Note 'errors' is an object with error types
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // If errors, return errors found by validator
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  // No erros found by validator
+  // Use Mongoose to find if the email is already in use
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // Use the error object from validations to send the error
+        errors.email = "Email already exists";
+        return res.status(400).json(errors);
+      } else {
+        // Passed validations, email not in use --> create new user
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      // Use the validations to send the error
+      errors.email = "User not found";
+      return res.status(404).json(errors);
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        // And here:
+        errors.password = "Incorrect password";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
 
 module.exports = router;
