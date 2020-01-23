a15fe72c2f2b3af62ee18dd81e9c0ceea87f32be
thejenkdog <thejenkdog@gmail.com>
2020-01-22 18:21:11 -0800

    Add mongoose import and connection to database

--- app.js
+++ app.js
@@ -3,7 +3,18 @@
 const express = require("express");
 const app = express();
 
-// Which port app runs on (process.env.port for Heroku)
+// Import Mongoose, import key to access database
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+
+// Connect to database through Mongoose
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+// Which port app runs on (process.env.port for Heroku, 5000 for local development)
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("hello world"));
