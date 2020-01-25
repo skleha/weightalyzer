116894340c56991576687daf7244aba4c54d3d7d
thejenkdog <thejenkdog@gmail.com>
2020-01-24 16:33:22 -0800

    Edits to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Import Mongoose, import key, connect to db
+// Import Mongoose, import key, connect to MongoDB
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 mongoose
@@ -6,32 +6,26 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-// Creates a new express server
+// Require express library, creates a new express server
 const express = require("express");
 const app = express();
+
+// Which port app runs on (process.env.port for Heroku, 5000 for local development)
+const port = process.env.PORT || 5000;
+
+// app initialized; port initialized; send message to indicate app is listening
+app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+// Response to a GET request to local host 5000
+app.get("/", (req, res) => res.send("hello world"));
+
+// Import defined APIs
+const users = require("./routes/api/users");
+
+// Tell express to use APIs
+app.use("/api/users", users);
 
 // So we can parse JSON sent to frontend
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-
-// Which port app runs on (process.env.port for Heroku, 5000 for local development)
-const port = process.env.PORT || 5000;
-
-// Import apis
-const users = require("./routes/api/users");
-
-// Tell express to use apis
-app.use("/api/users", users);
-
-
-
-
-// Response to a GET request to local host 5000
-app.get("/", (req, res) => res.send("hello world"));
-
-
-// If the app is listening, then note so in the server log
-app.listen(port, () => console.log(`Server is running on port ${port}`))
