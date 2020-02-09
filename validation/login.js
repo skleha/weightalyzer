c2a3873a50fc7621be8e8f8889824479c4a3832c
thejenkdog <thejenkdog@gmail.com>
2020-02-09 15:46:44 -0800

    Complete first draft of weight validation

--- validation/login.js
+++ validation/login.js
@@ -5,7 +5,6 @@ const validText = require('./valid-text');
 module.exports = function validateLoginInput(data)  {
   let errors = {};
 
-  // Reset email/password to '' if user entry doesn't pass the validText test
   data.email = validText(data.email) ? data.email : '';
   data.password = validText(data.password) ? data.password: '';
 
--- validation/weightValidation.js
+++ validation/weightValidation.js
@@ -1,10 +1,29 @@
 const Validator = require('validator');
 const validText = require('./valid-text');
+const validDate = require('./valid-date');
+const validNum = require('./valid-num');
 
 module.exports = function validateWeightInput(data) {
-  data.userId = validText(data.userId) ? data.userId : '';
-  data.weight = 
+  // data.userId = validText(data.userId) ? data.userId : '';
+  // data.date = validDate(data.date) ? data.weight : '';
+  // data.weight = validNum(data.weight) ? data.weight : '';
+  let errors = {};
 
+  if (Validator.isEmpty(data.userId)) {
+    errors.userId = "User field is required";
+  }
 
+  if (Validator.isEmpty(data.date)) {
+    errors.date = "Date field is required";
+  }
+
+  if (Validator.isEmpty(data.weight)) {
+    errors.weight = "Weight field is required";
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  }
 
 }
