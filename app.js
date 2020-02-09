fe6acd594999dba690e010f80c318a05e42c121e
thejenkdog <thejenkdog@gmail.com>
2020-02-09 14:59:58 -0800

    Add route to app.js, minor edits to weights routes

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const express = require("express");
 const app = express(); // Initialize Express server
 const bodyParser = require("body-parser");  // Middlewear for parsing the request body
 const port = process.env.PORT || 5000; // Which port app runs on (process.env.port for Heroku, 5000 for local development)
-const users = require("./routes/api/users");
 const passport = require('passport');
+const users = require("./routes/api/users");
+const weights = require("./routes/api/weights");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -20,11 +21,12 @@ app.use(bodyParser.json());
 // Send message to indicate app is listening
 app.listen(port, () => console.log(`Server is running on port ${port}`))
 
-// Middleware that moves 
+// Middleware facilitates jwt
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
 // Tell express to use APIs with prefix "/api/users"
 // Any endpoint defined in users will need this prefix
 app.use("/api/users", users);
+app.use("/api/weights", weights);
 
--- routes/api/weights.js
+++ routes/api/weights.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Weight = require('../../models/Weight');
 
 // Test route
-router.get('/test', (req, res) => res.json({msg: "this is the users route"}))
+router.get('/test', (req, res) => res.json({msg: "this is the weights route"}));
 
 // Add weight
 
