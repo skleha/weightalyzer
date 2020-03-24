e9fffbcb04e8c24fd1c696c0265a5001d29180a7
thejenkdog <thejenkdog@gmail.com>
2020-03-23 17:15:37 -0700

    Install dependencies, react-redux, react-router-dom

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
-
 import MainPage from './main/main_page';
+
+
 
 const App = () => (
   <Switch>
