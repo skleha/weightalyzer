6c1cb95d09915248e2284eceaf297215f8a84c7f
thejenkdog <thejenkdog@gmail.com>
2020-03-23 17:54:37 -0700

    Add signup route

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import MainPage from './main/main_page';
+import SignUpContainer from './auth/SignUpFormContainer';
+
 
 
 
 const App = () => (
   <Switch>
     <AuthRoute exact path="/" component={MainPage} />
+    <AuthRoute exact path="/signup" component={SignUpContainer} />
+    
   </Switch>
 );
 
---
+++ frontend/src/components/auth/SignUpFormContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SignUpFormContainer extends React.Component {
+  render() {
+    return <div>You've got the signup form</div>;
+  }
+}
+
+export default SignUpFormContainer;
