1b1283f00fa28b4c4f39d1f87cfcf25fb8d423b1
thejenkdog <thejenkdog@gmail.com>
2020-03-30 17:01:37 -0700

    styling for text inputs

--- frontend/src/components/auth/SignUpForm.jsx
+++ frontend/src/components/auth/SignUpForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { signup } from '../../actions/session_actions';
-
+import '../../stylesheets/auth.css';
 
 const SignUpForm = props => {
 
