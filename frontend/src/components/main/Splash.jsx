075b71976e77a8393d7d17b2b67244dee792aadb
thejenkdog <thejenkdog@gmail.com>
2020-04-03 11:30:10 -0700

    Add buttons, navigation and styling to splash page

--- frontend/src/components/main/Splash.jsx
+++ frontend/src/components/main/Splash.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-const Splash = () => {
+const Splash = props => {
+
+  const handleRegisterClick = () => {
+    props.history.push("/register");
+  }
+
+  const handleLoginClick = () => {
+    props.history.push("/login");
+  }
 
   return (
-    <div className="splash">
-      A simple application to keep track of your weight over time.
+    <div>
+    
+      <div className="splash-copy">
+        A simple application to keep track of your weight over time.
+      </div>
+    
+      <div className="splash-button-tray">
+        <button onClick={handleRegisterClick}>Register</button>
+        <button onClick={handleLoginClick}>Login</button>
+      </div>
+
     </div>
   )
 }
