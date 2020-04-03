ea195d07af6c7859f9e1b2311c5b914cf93b6044
thejenkdog <thejenkdog@gmail.com>
2020-04-03 16:19:09 -0700

    recharts tested successfully

--- frontend/src/components/main/WeightView.jsx
+++ frontend/src/components/main/WeightView.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+
+const data = [
+  { name: "Page A", uv: 100 },
+  { name: "Page B", uv: 200 },
+  { name: "Page C", uv: 300 }
+];
 
 const WeightView = () => {
   
   
-
-
-
-
+  
 
   return (
-    <div>Weight views</div>
+    <LineChart width={300} height={300} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+  </LineChart>    
 
   )
   
