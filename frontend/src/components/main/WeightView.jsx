6d688dbc6b21f09503cb5002a435102512881890
thejenkdog <thejenkdog@gmail.com>
2020-04-03 17:48:46 -0700

    Styling of graph

--- frontend/src/components/main/WeightView.jsx
+++ frontend/src/components/main/WeightView.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import { LineChart, Line, XAxis, YAxis } from 'recharts';
 
 const data = [
-  { name: "Page A", uv: 100 },
-  { name: "Page B", uv: 200 },
-  { name: "Page C", uv: 300 }
+  { name: "Page A", uv: 140 },
+  { name: "Page B", uv: 145 },
+  { name: "Page C", uv: 150 }
 ];
 
 const WeightView = () => {
-  
-  
+
   
 
+
+
   return (
-    <div className="something">
-      <LineChart width={300} height={300} data={data}>
+    <div className="weight-graph-container">
+      <LineChart
+        className="weight-graph"
+        width={330}
+        height={300}
+        data={data}
+        margin={{ top: 5, right: 10, bottom: 5, left: 0 }}
+      >
         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <XAxis dataKey="name" />
+        <YAxis />
       </LineChart>
     </div>
   );
